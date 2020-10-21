a2b9808df6e9aa65f40161c25104a4199ae3f569
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-10-20 22:01:51 -0400

    Array Intro

--- src/com/syntax/class09/NestedLoopDemo.java
+++ src/com/syntax/class09/NestedLoopDemo.java
@@ -64,34 +64,38 @@ public class NestedLoopDemo {
 				}
 			}
 		}
-		
-		System.out.println(" ------------ how can I print clock ---------------------");
+
+		System.out.println(" ------------ how can I print clock another way---------------------");
 
 		for (int h = 0; h < 24; h++) {
 
 			for (int m = 0; m < 60; m++) {
-				
-				if (h<10 && m<10) {
+
+				if (h < 10 && m < 10) {
 					System.out.println("0" + h + ":0" + m);
-					
-				}else if (h<10 && m>10) {
+
+				} else if (h < 10 && m > 10) {
 					System.out.println("0" + h + ":" + m);
+				} else if (h > 10 && m < 10) {
+					System.out.println(h + ":0" + m);
+				} else {
+					System.out.println(h + ":" + m);
 				}
-				
-			}}
+
+			}
+		}
 
 		System.out.println(" ------------ how can I print car odometer ---------------------");
-		
-		 for(int a=0; a<10; a++) {
-			 for(int b=0; b<10; b++) {
-				 for(int c=0; c<10;c++) {
-					 for(int d=0;d<10; d++) {
-						 for(int e=0;e<10;e++) {
-							 System.out.println(a+""+b+""+c+""+d+""+e);
-						 }
-					 }
-				 }
-			 }
-		 }
+
+		for (int a = 0; a < 10; a++) {
+			for (int b = 0; b < 10; b++) {
+				for (int c = 0; c < 10; c++) {
+					for (int d = 0; d < 10; d++) {
+							System.out.println(a + "" + b + "" + c + "" + d);
+
+					}
+				}
+			}
+		}
 	}
 }
