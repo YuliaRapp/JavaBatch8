96cb4b806465cee1358c7f2065d4f7aeca7bd8c2
Asghar Nazir <asgharnazir@datawiz.ai>
2020-10-28 19:00:03 -0400

    Just a test

--- src/com/syntax/class13/ClassesInJava.java
+++ src/com/syntax/class13/ClassesInJava.java
@@ -2,6 +2,7 @@ package com.syntax.class13;
 
 public class ClassesInJava {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hello");
+
     }
 }
