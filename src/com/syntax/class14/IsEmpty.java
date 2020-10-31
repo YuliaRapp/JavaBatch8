975a45ee1d568e841689b79387a8c53b739842f4
Asghar Nazir <asgharnazir@datawiz.ai>
2020-10-31 14:41:28 -0400

    class 15

--- src/com/syntax/class14/IsEmpty.java
+++ src/com/syntax/class14/IsEmpty.java
@@ -3,7 +3,7 @@ package com.syntax.class14;
 public class IsEmpty {
 
     public static void main(String[] args) {
-        String name = "Mujtaba";
+         String name = "Mujtaba";
 
         System.out.println(name.isEmpty());
         name="";
