0b38d168757bcb45029ed761afca6ed82e55dfc3
Asghar Nazir <asgharnazir@datawiz.ai>
2020-11-18 22:03:21 -0500

    Abstraction

---
+++ src/com/syntax/class27/VehicleTester.java
@@ -0,0 +1,14 @@
+package com.syntax.class27;
+
+public class VehicleTester {
+
+
+
+    public static void main(String[] args) {
+//        Vehicle vehicle= new Vehicle();
+//        Car cvar= new Car(); abstract class can not create objects
+        Vehicle tesla=new Tesla("Red","x","tesla");
+
+
+    }
+}
