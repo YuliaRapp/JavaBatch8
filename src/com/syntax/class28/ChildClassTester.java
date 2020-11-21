a542a6c86637496a0c46a5b1753fe95a22f1f360
Asghar Nazir <asgharnazir@datawiz.ai>
2020-11-21 11:23:07 -0500

    Interface

---
+++ src/com/syntax/class28/ChildClassTester.java
@@ -0,0 +1,14 @@
+package com.syntax.class28;
+
+public class ChildClassTester {
+
+
+    public static void main(String[] args) {
+       ChildClass childClass = new ChildClass();
+       childClass.printInfo();
+
+       // MyInterface.name="something"; //fields can not be reassigned as they are always final
+       MyInterface myInterface=new ChildClass();
+       myInterface.printInfo();
+    }
+}
