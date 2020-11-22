8d36e19c69f1694139c18a8eb60a29480081b431
Asghar Nazir <asgharnazir@datawiz.ai>
2020-11-22 12:12:40 -0500

    Encapsulation

---
+++ src/com/syntax/class29/GUIAppPart2.java
@@ -0,0 +1,17 @@
+package com.syntax.class29;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIAppPart2 extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
