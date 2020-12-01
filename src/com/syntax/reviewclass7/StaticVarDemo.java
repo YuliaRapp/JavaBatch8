07bb8d8a6fe9499e18a0a84e0c665e1c937a7735
Asghar Nazir <asgharnazir@datawiz.ai>
2020-11-30 21:52:41 -0500

    Collections Iterator

--- src/com/syntax/reviewclass7/StaticVarDemo.java
+++ src/com/syntax/reviewclass7/StaticVarDemo.java
@@ -1,18 +1,28 @@
 package com.syntax.reviewclass7;
 
-class Student{
+class Student {
     static int totalStudentCount;
-    static final String schoolName="Syntax";
+    static final String schoolName = "Syntax";
     String name;
-    final static int MAX_STUDENTS_ALLOWED=3;
-    Student(){
+    final static int MAX_STUDENTS_ALLOWED = 3;
+    private static Student student = null;
+
+    private Student() {
         totalStudentCount++;
     }
 
-    void printInfo(){
+    static Student getInstance() {
+        if (student == null) {
+            student = new Student();
+        }
+        return student;
+
+    }
+
+    void printInfo() {
         System.out.println("Name of the student "
-                +name+" Name of the school "
-                +schoolName+" Total Students registered "+totalStudentCount);
+                + name + " Name of the school "
+                + schoolName + " Total Students registered " + totalStudentCount);
     }
 }
 
@@ -21,26 +31,41 @@ public class StaticVarDemo {
 
     public static void main(String[] args) {
 
-        School school=new School();
-        Student student1= new Student();
-        student1.name="Ali";
+
+        Student student5 = Student.getInstance();
+        student5.name = "Mahmut";
+        student5.printInfo();
+
+        Student student6 = Student.getInstance();
+        student6.name = "Davit";
+        student6.printInfo();
+        System.out.println(student5.name);
+        System.out.println(student6.name);
+
+       // Student student=new Student();
+     /*   School school = new School();
+        Student student1 = new Student();
+        student1.name = "Ali";
         student1.printInfo();
         school.addStudent(student1);
-        Student student2= new Student();
-        student2.name="Khaliqyar";
+        Student student2 = new Student();
+        student2.name = "Khaliqyar";
         student2.printInfo();
         school.addStudent(student2);
-        Student student3= new Student();
-        student3.name="Danilo";
+        Student student3 = new Student();
+        student3.name = "Danilo";
         student3.printInfo();
         school.addStudent(student3);
-        Student student4= new Student();
-        student4.name="Fahim";
+        Student student4 = new Student();
+        student4.name = "Fahim";
         student4.printInfo();
         school.addStudent(student4);
-        Student student5= new Student();
-        student5.name="Mahmut";
-        student5.printInfo();
+        Student student5 = new Student();
+        student5.name = "Mahmut";
+        student5.printInfo();*/
+
+
+
 
 
     }
