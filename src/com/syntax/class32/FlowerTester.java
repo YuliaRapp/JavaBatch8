d04df4d4385867870f86a629bafbc6277f87ac6f
Asghar Nazir <asgharnazir@datawiz.ai>
2020-12-05 11:41:21 -0500

    Sets and Maps

---
+++ src/com/syntax/class32/FlowerTester.java
@@ -0,0 +1,21 @@
+package com.syntax.class32;
+
+import java.util.ArrayList;
+
+public class FlowerTester {
+    public static void main(String[] args) {
+
+        ArrayList<Flower> flowerArrayList=new ArrayList<>();
+        flowerArrayList.add(new SunFlower("Sunflower","Yellow"));
+        flowerArrayList.add(new Tulip("Tulip","Red"));
+        flowerArrayList.add(new Rose("Rose","Pink"));
+
+        for (Flower flower:flowerArrayList
+             ) {
+            System.out.println("Name "+flower.name+" Color "+flower.color);
+            flower.bloom();
+            System.out.println();
+        }
+
+    }
+}
