5a10aa4093a93d4c2293b150fb2ed22032835b55
Asghar Nazir <asgharnazir@datawiz.ai>
2020-12-13 10:55:22 -0500

    driver

---
+++ src/com/syntax/class35/PropertiesFileHandling.java
@@ -0,0 +1,10 @@
+package com.syntax.class35;
+
+public class PropertiesFileHandling {
+    public static void main(String[] args) {
+
+        String baseAddress = System.getProperty("user.dir");
+        System.out.println(baseAddress);
+
+    }
+}
