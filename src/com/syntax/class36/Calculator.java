1046ef2f70baf751b63305ab9617aea74af63087
Asghar Nazir <asgharnazir@datawiz.ai>
2020-12-19 14:58:27 -0500

    Excel file reading and writing

---
+++ src/com/syntax/class36/Calculator.java
@@ -0,0 +1,7 @@
+package com.syntax.class36;
+
+public class Calculator {
+    public  static void calculate(){
+        System.out.println(10/0);
+    }
+}
---
+++ src/com/syntax/class36/Exception.java
@@ -0,0 +1,23 @@
+package com.syntax.class36;
+
+public class Exception {
+    public static void main(String[] args) {
+
+        System.out.println("important line of code");
+        System.out.println("Normal line of code");
+        int num1=10;
+        int num2=5;
+
+        if(num2!=0){
+            System.out.println(num1/num2);
+        }else {
+            System.out.println("Operation not allowed");
+        }
+
+
+        System.out.println("Important line of code");
+
+    }
+
+
+}
