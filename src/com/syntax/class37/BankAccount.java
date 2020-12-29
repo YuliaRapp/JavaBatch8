bd5a26dd6b0368de70b5a14a1f93879344e8c7b7
Asghar Nazir <asgharnazir@datawiz.ai>
2020-12-28 21:49:09 -0500

    Review class 16

--- src/com/syntax/class37/BankAccount.java
+++ src/com/syntax/class37/BankAccount.java
@@ -1,10 +1,13 @@
 package com.syntax.class37;
 
+import java.util.*;
+
 public class BankAccount {
     public static double balance=10;
     public static void transferBalance(double transferAmount) throws InsufficientBalanceException {
         if(transferAmount>balance){
             throw new InsufficientBalanceException("Not enough balance");
         }
+
     }
 }
