420fc5b6253cdacd2a42d7e4a2ce861152abfcca
Asghar Nazir <asgharnazir@datawiz.ai>
2021-01-04 21:22:21 -0500

    Review class 17

---
+++ src/com/syntax/reviewclass17/ApplicationsOfSets.java
@@ -0,0 +1,23 @@
+package com.syntax.reviewclass17;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class ApplicationsOfSets {
+    public static void main(String[] args) {
+
+        // why and when we should be using the sets
+        Scanner scanner=new Scanner(System.in);
+        ArrayList<String> arrayList=new ArrayList<>();
+        TreeSet<String> treeSet=new TreeSet<>();
+
+        for (int i = 0; i < 5; i++) {
+            treeSet.add(scanner.next());
+        }
+        arrayList.addAll(treeSet);
+        System.out.println(arrayList);
+        String name="asdmfbhsd";
+
+    }
+}
