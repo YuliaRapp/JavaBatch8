ddf534b89654b8a2768174b53a399a87262a475a
Asghar Nazir <asghar@syntaxtech.com>
2021-01-11 21:05:27 -0500

    Review class 20

--- src/com/syntax/reviewclass16/LambdaDemo.java
+++ src/com/syntax/reviewclass16/LambdaDemo.java
@@ -2,9 +2,6 @@ package com.syntax.reviewclass16;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-
 public class LambdaDemo {
     public static void main(String[] args) {
 
