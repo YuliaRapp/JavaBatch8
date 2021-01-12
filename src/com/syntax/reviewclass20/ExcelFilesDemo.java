cb890e280f386d0e09ea20cf4f0ac90117605ade
Asghar Nazir <asghar@syntaxtech.com>
2021-01-11 21:05:00 -0500

    Review class 20

---
+++ src/com/syntax/reviewclass20/ExcelFilesDemo.java
@@ -0,0 +1,35 @@
+package com.syntax.reviewclass20;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelFilesDemo {
+    public static void main(String[] args) throws IOException {
+        String path="C:\\Users\\Asghar Nazir\\IdeaProjects\\JavaBatch8\\src\\com\\syntax\\reviewclass20\\Test.xlsx";
+        FileInputStream fileInputStream=new FileInputStream(path);
+        XSSFWorkbook workbook=new XSSFWorkbook(fileInputStream);
+        Sheet sheet=workbook.getSheet("Sheet1");
+
+        List<Person> allFileData=new ArrayList<>();
+        for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row=sheet.getRow(i);
+            Person person=new Person();
+            person.setFirstName(row.getCell(0).toString());
+            person.setLastName(row.getCell(1).toString());
+            person.setAge( (int)row.getCell(2).getNumericCellValue());
+            allFileData.add(person);
+        }
+
+        System.out.println(allFileData);
+
+
+
+    }
+}
